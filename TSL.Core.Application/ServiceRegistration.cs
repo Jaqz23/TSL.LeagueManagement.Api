42a02a132848ad4b71c38123116caca0e1116b8b
Jaqz23 <gustavojaquezr23@gmail.com>
2026-02-09 11:10:27 -0400

    Add project files.

---
+++ TSL.Core.Application/ServiceRegistration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSL.Core.Application
+{
+    internal class ServiceRegistration
+    {
+    }
+}
