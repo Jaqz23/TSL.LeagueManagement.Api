8e7858c6507f73c9feab6c70294c2d27ef1d8800
Jaqz23 <gustavojaquezr23@gmail.com>
2026-02-12 15:01:34 -0400

    Add generic repository and relocate Unit of work

---
+++ TSL.Infrastructure.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using TSL.Core.Application.Interfaces.Repositories;
+using TSL.Infrastructure.Persistence.Contexts;
+
+namespace TSL.Infrastructure.Persistence.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        protected readonly ApplicationContext _context;
+        protected readonly DbSet<TEntity> _dbSet;
+
+        public GenericRepository(ApplicationContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _dbSet = _context.Set<TEntity>();
+        }
+
+        #region Consultas
+
+        public virtual async Task<List<TEntity>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public virtual async Task<List<TEntity>> GetAllAsNoTrackingAsync()
+        {
+            return await _dbSet.AsNoTracking().ToListAsync();
+        }
+
+        public virtual async Task<TEntity?> GetByIdAsync(int id) 
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public virtual async Task<List<TEntity>> FindAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            return await _dbSet.Where(filter).ToListAsync();
+        }
+
+        public virtual async Task<List<TEntity>> FindWithIncludeAsync(
+            Expression<Func<TEntity, bool>> filter,
+            params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            IQueryable<TEntity> query = _dbSet;
+
+            // Aplicar includes
+            foreach(var includeProperty in includeProperties) 
+            {
+                query = query.Include(includeProperty);
+            }
+
+            // Aplicar filtro
+            return await query.Where(filter).ToListAsync();
+
+        }
+
+        public virtual async Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            return await _dbSet.AnyAsync(filter);
+        }
+
+        public virtual async Task<int> CountAsync(Expression<Func<TEntity, bool>>? filter = null) 
+        {
+            if(filter == null) 
+            {
+                return await _dbSet.CountAsync();
+            }
+
+            return await _dbSet.CountAsync(filter);
+        }
+
+        public virtual async Task<TEntity?> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            return await _dbSet.FirstOrDefaultAsync(filter);
+        }
+
+        #endregion
+
+
+        #region Comandos
+
+        public virtual async Task AddAsync(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public Task AddRangeAsync(IEnumerable<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public void UpdateRange(IEnumerable<TEntity> entities)
+        {
+            _dbSet.UpdateRange(entities);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+
+        #endregion
+
+    }
+}
