de84d7c8f9cec3482a64ffc98242e1bd0cc4b893
Jaqz23 <gustavojaquezr23@gmail.com>
2026-03-24 09:40:34 -0400

    Add Identity main structure

---
+++ TSL.Infrastructure.Identity/Settings/JWTSettings.cs
@@ -0,0 +1,12 @@
+﻿
+namespace TSL.Infrastructure.Identity.Settings
+{
+    public class JWTSettings
+    {
+        public string Key { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public int DurationInMinutes { get; set; }
+
+    }
+}
---
+++ TSL.WebApi/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TSL.WebApi.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
